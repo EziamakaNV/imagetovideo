7e753cf56886f4e1d87062b41bd7547b60c533b3
Nnaemeka Eziamaka <eziamakanv@gmail.com>
2020-01-29 01:38:20 +0100

    another commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const path = require('path');
-const imageToVideo = require('./videoshow');
+const imageToVideo = require('./image_to_video');
 
 // ensure the pictures are
 const imagePathArray = [
     path.join(process.cwd(), 'images', 'resize1.jpg'),
+    path.join(process.cwd(), 'images', 'resize2.jpg'),
+    path.join(process.cwd(), 'images', 'resize3.jpg'),
+    path.join(process.cwd(), 'images', 'resize4.jpg'),
+    path.join(process.cwd(), 'images', 'resize1.jpg'),
+    path.join(process.cwd(), 'images', 'resize2.jpg'),
+    path.join(process.cwd(), 'images', 'resize3.jpg'),
+    path.join(process.cwd(), 'images', 'resize4.jpg'),path.join(process.cwd(), 'images', 'resize1.jpg'),
+    path.join(process.cwd(), 'images', 'resize2.jpg'),
+    path.join(process.cwd(), 'images', 'resize3.jpg'),
+    path.join(process.cwd(), 'images', 'resize4.jpg'),path.join(process.cwd(), 'images', 'resize1.jpg'),
+    path.join(process.cwd(), 'images', 'resize2.jpg'),
+    path.join(process.cwd(), 'images', 'resize3.jpg'),
+    path.join(process.cwd(), 'images', 'resize4.jpg'),path.join(process.cwd(), 'images', 'resize1.jpg'),
+    path.join(process.cwd(), 'images', 'resize2.jpg'),
+    path.join(process.cwd(), 'images', 'resize3.jpg'),
+    path.join(process.cwd(), 'images', 'resize4.jpg'),path.join(process.cwd(), 'images', 'resize1.jpg'),
     path.join(process.cwd(), 'images', 'resize2.jpg'),
     path.join(process.cwd(), 'images', 'resize3.jpg'),
     path.join(process.cwd(), 'images', 'resize4.jpg'),
